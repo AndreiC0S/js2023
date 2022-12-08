bb7ab088263666d66fd9902796d5b93aba04c8f1
AndreiCos <frontendjavascript@C3-02.local>
2022-12-08 20:44:03 +0200

    js

--- curs1/ap1.js
+++ curs1/ap1.js
@@ -1,1 +1,9 @@
-hhhh
+
+alert("salut JavraScript")
+window.alert('salut')
+confirm("vreau sa invat js")
+console.log("mesaj in ocnsola")
+console.info("info status")
+console.error("mesaj de eroare")
+
+
