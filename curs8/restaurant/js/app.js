b07f446e13874cd6fac39f66e43771901541b170
AndreiCos <frontendjavascript@C3-02.local>
2023-01-17 20:58:04 +0200

    f

--- curs8/restaurant/js/app.js
+++ curs8/restaurant/js/app.js
@@ -5,39 +5,73 @@ const sectionCenter = document.querySelector(".section-center");
 const btnContainer = document.querySelector(".btn-container");
 // display all items when page loads
 window.addEventListener("DOMContentLoaded", function () {
-    diplayMenuItems(menu);
-    displayMenuButtons();
+  displayMenuItems(menu);
+  displayMenuButtons();
 });
 
-function diplayMenuItems(menuItems) {
-   const categories = menu.reduce(
-    function (values, item){
-        if(!values.includes(item.category)){
-            values.push(item.category);
-        }
-        return values;
-    },["all"]
-   );
-   const categoryBtns = categories.map(function(category,index){
-        let btn_nr = 'btn' + (index + 1);
-        const categoryRo = {
-            all:'Tot',
-            breakfast:'Mic Dejun',
-            lunch:'Pranz',
-            dinner:'Cina',
-            shakes:'Bauturi',
-        }
-return `<button type="button" class="filter-btn" data-id=${category} data-nr=${btn_nr}>
-    ${categoryRo[category]}
-    </button>`
-   })
-   .join("");
+function displayMenuItems(menuItems) {
+    let displayMenu = menuItems.map(function (item){
+        return`<article class="menu-item">
+            <img src="${item.img}" alt=${item.title} class="photo" />
+            <div class="item-info">
+                <header>
+                <h4>${item.title}</h4>
+                <h4 class="price">${item.price} RON</h4>
+                </header>
+                <p class="item-text">
+                ${item.desc}
+                </p>
+            </div>
+            </article>`;
+    })
+    displayMenu = displayMenu.join("");
+    sectionCenter.innerHTML = displayMenu;
+}
 
-    btnContainer.innerHTML = categoryBtns;
-//    const filterBtn = btnContainer.querySelectorAll(".filter-btn");
-//    console.log(filterBtn);
-}
+
 function displayMenuButtons() {
-    
-   
+  const categories = menu.reduce(
+    function (values, item) {
+      if (!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    },
+    ["all"]
+  );
+  const categoryBtns = categories
+    .map(function (category, index) {
+      let btn_nr = "btn" + (index + 1);
+      const categoryRo = {
+        all: "Tot",
+        breakfast: "Mic Dejun",
+        lunch: "Pranz",
+        dinner: "Cina",
+        shakes: "Bauturi",
+      };
+      return `<button type="button" class="filter-btn" data-id=${category} data-nr=${btn_nr}>
+    ${categoryRo[category]}
+    </button>`;
+    })
+    .join("");
+
+  btnContainer.innerHTML = categoryBtns;
+
+  const filterBtn = btnContainer.querySelectorAll(".filter-btn");
+  filterBtn.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter(function (menuItem) {
+        if (menuItem.category == category) {
+          return menuItem;
+        }
+      });
+      if (category === "all") {
+        displayMenuItems(menu);
+      } else {
+        displayMenuItems(menuCategory);
+      }
+    });
+  });
 }
+
